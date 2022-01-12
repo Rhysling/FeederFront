1de23be7ad8a3ee0a25de556eaa0d0c0a69191fd
Rhysling <rkummer@polson.com>
2022-01-12 13:18:17 -0800

    Menu transition. Record build log.

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { slide } from 'svelte/transition';
   import { routes, currentRoute, navTo } from "../stores/route-store.js";
 
   let isAuthenticated = true;
@@ -12,7 +13,7 @@
 
 <div class="ap-menu">
   {#if !showAdmin}
-    <div class="ap-menu-main">
+    <div transition:slide class="ap-menu-main">
       <div class="ap-menu-heading"><a href="/">Twit Feeder</a></div>
       <a class="ap-menu-logo" href="/"><img src="/assets/img/logo-twitfeeder-400x400.png" alt="TwitFeeder" title="Twitter and other feeds sent to your RSS reader" /></a>
       {#if isAuthenticated}
@@ -22,15 +23,15 @@
 
       {#if isAuthenticated}
         <a href="/Manage/MyFeeds">My Feeds</a>
-        <a href="/Manage/Account">Account</a>
+        <a href="/" on:click={(e) => {e.preventDefault(); showAdmin = !showAdmin; }}>Account</a>
         <a href="/Account/LogOff">Sign Out</a>
       {:else}
         <a href="/Account/Login">Sign In/Sign Up</a>
       {/if}
     </div>
   {:else}
-    <div class="ap-menu-admin">
-      <a href="/">&lt; Back</a>
+    <div transition:slide class="ap-menu-admin">
+      <a href="/" on:click={(e) => {e.preventDefault(); showAdmin = !showAdmin; }}>&lt; Back</a>
       <div class="ap-menu-sep">&nbsp;</div>
       <a href="/Admin/Users">Users</a>
       <a href="/Admin/Log">Log</a>
@@ -47,7 +48,7 @@
     top: 0;
 		left: 0;
   }
-  .ap-menu-main {
+  .ap-menu-main, .ap-menu-admin {
     display: flex;
     flex-flow: row nowrap;
     align-items: baseline;
